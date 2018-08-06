9b99b7ba3e4ef6048259b008cc8604eabb94eee6
Peerity <jan@peerity.io>
2018-08-06 19:59:07 +0200

    file input

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -5,24 +5,46 @@
          <div class="panel-heading">
             <h3 class="panel-title">Add User</h3>
          </div>
+
          <div class="panel-body">
             <form>
                <div class="form-group">
                   <label for="name">Name</label>
                   <input type="text" class="form-control" placeholder="Name" v-model="name">
                </div>
+
                <div class="form-group">
                   <label for="email">Email</label>
                   <input type="text" class="form-control" placeholder="Email" v-model="email">
                </div>
+
+               <div class="form-group">
+                   <label class="file-label">
+                        <input class="file-input" type="file" @blur="getFileName">
+                        <span class="file-cta">
+                            <span class="file-icon">
+                                <i class="fa fa-upload"></i>
+                            </span>
+                            <span class="file-label">
+                                Choose a file…
+                            </span>
+                        </span>
+                        <span class="file-name">
+                            {{ fname }}
+                        </span>
+                    </label>
+               </div>
+
                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
          </div>
       </div>
+
       <div class="panel panel-success"> <br>
          <div class="panel-body">
             My Name is : {{ name }} <br>
-            My Email is : {{ email }}
+            My Email is : {{ email }} <br>
+            Filename: {{ fname }} <br> <br> <br> <br>
          </div>
       </div>
    </div>
@@ -33,8 +55,18 @@
        data () {
           return {
                name: '',
-               email: ''
+               email: '',
+               fname: ''
           }
-       }
+      },
+
+      methods: {
+      getFileName (e) {
+            this.fpath = e.target.files[0];
+            if (this.fpath){
+                this.fname = this.fpath.name;
+            }
+        }
+      }
    }
 </script>
