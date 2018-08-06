4004f2421ae4ead1a347839ea5c2ec97eb1b5aa4
Peerity <jan@peerity.io>
2018-08-06 16:03:09 +0200

    vue cli 3

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page for</h1>
+    <Ether msg="Ethereum Lovers...." />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Ether from "@/components/Ether.vue";
+
+export default {
+  name: "ether",
+  components: {
+    Ether
+  }
+};
+</script>
---
+++ src/views/Bitcoin.vue
@@ -0,0 +1,26 @@
+
+<template>
+   <div>
+      <h1>
+          {{ price }} in {{ clicks }} clicks.
+      </h1>
+      <button @click="clicking()">Change Value</button>
+  </div>
+</template>
+
+<script>
+   export default {
+   data() {
+         return {
+            price: 200,
+            clicks: 0
+         };
+      },
+   methods: {
+      clicking() {
+         this.price = this.price + 300;
+         this.clicks = this.clicks + 1;
+      }
+   }
+  }
+</script>
